39a2269fdc616551e208c1be9147e5e2d5cf0586
Suparno <suparno.bose1@wipro.com>
2019-10-09 14:08:23 +0530

    CocoaPod initiated. ObjectMapper framework added.

--- Info-App/Info-App/ViewControllers/HomeViewController.swift
+++ Info-App/Info-App/ViewControllers/HomeViewController.swift
@@ -12,6 +12,17 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        NetworkManger.getInfoData { (info, error) in
+            if let error = error{
+                
+            }
+            else{
+                
+            }
+        }
+    }
+    
+    func setupUI(){
         self.view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.4, alpha: 1.0)
     }
 
---
+++ Info-App/Pods/Target Support Files/Pods-Info-AppTests/Pods-Info-AppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Info_AppTests : NSObject
+@end
+@implementation PodsDummy_Pods_Info_AppTests
+@end

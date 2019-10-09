5f8dc04fb2009a170a0175b157f5b4aa1ad4347e
Suparno <suparno.bose1@wipro.com>
2019-10-09 18:08:15 +0530

    SDWebImage pod added to the project

--- Info-App/Info-App/ViewControllers/HomeViewController.swift
+++ Info-App/Info-App/ViewControllers/HomeViewController.swift
@@ -7,25 +7,37 @@
 //
 
 import UIKit
+import SDWebImage
 
 class HomeViewController: UIViewController {
 
+    weak var lightsMeaningLabel: UILabel!
+    weak var circularBGView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        NetworkManger.getInfoData { (info, error) in
-            if let error = error{
-                
-            }
-            else{
-                
-            }
-        }
+        setupUI()
+        fetchInfoData()
     }
     
     func setupUI(){
         self.view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.4, alpha: 1.0)
     }
-
+    
+    func updateUI() {
+        
+    }
+    
+    func fetchInfoData() {
+        NetworkManger.getInfoData { (info, error) in
+            if error != nil{
+                
+            }
+            else{
+                self.updateUI()
+            }
+        }
+    }
 
 }
 

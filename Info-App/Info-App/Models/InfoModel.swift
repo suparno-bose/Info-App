8958757aebddf301fc9bd211fa6b01c9078486f7
Suparno <suparno.bose1@wipro.com>
2019-10-11 19:28:12 +0530

    New Open source flat Font added. License added to the project HexColor library added to the pod

--- Info-App/Info-App/Models/InfoModel.swift
+++ Info-App/Info-App/Models/InfoModel.swift
@@ -11,8 +11,8 @@ import ObjectMapper
 
 class InfoModel: Mappable {
     
-    var title : String?
-    var description : String?
+    var title : String = ""
+    var description : String = ""
     var imageHref : String?
     
     convenience required init?(map: Map) {

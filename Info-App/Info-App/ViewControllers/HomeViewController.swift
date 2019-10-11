1a7c346a9a3e9c3f61aae25b52c47f60cab3ab80
Suparno <suparno.bose1@wipro.com>
2019-10-11 18:49:40 +0530

    Removing the Stand-alone navigation bar to get the ease of Navigation controller Navigation bar

--- Info-App/Info-App/ViewControllers/HomeViewController.swift
+++ Info-App/Info-App/ViewControllers/HomeViewController.swift
@@ -8,44 +8,56 @@
 
 import UIKit
 import SDWebImage
+import SnapKit
 
 class HomeViewController: UIViewController {
 
-    var navigationBar: UINavigationBar!
-    var factTableView: UITableView!
-    
+    // MARK: - UI variables
+    var navigationBar   : UINavigationBar!
+    var titleView       : UILabel!
+    var factTableView   : UITableView!
+    // MARK: - Data variables
     var responseData : ResponseModel?
     
+    // MARK: - Life-cycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         fetchInfoData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    // MARK: - UI related methods
     func setupUI(){
         self.view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.4, alpha: 1.0)
         
-        let navigationBarHeight = 60 + UIApplication.shared.statusBarFrame.size.height
-        navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0,
-                                                      width: self.view.frame.width, height: navigationBarHeight))
-        self.view.addSubview(navigationBar)
-        
-        factTableView = UITableView(frame: CGRect(x: 0, y: navigationBarHeight,
-                                                  width: self.view.frame.width,
-                                                  height: self.view.frame.height - navigationBarHeight))
-        factTableView.dataSource = self
-        factTableView.delegate = self
+        factTableView = UITableView(frame: CGRect.zero)
+        factTableView.backgroundColor = UIColor.clear
         self.view.addSubview(factTableView)
+        factTableView.translatesAutoresizingMaskIntoConstraints = false
+        factTableView.snp.makeConstraints { (make) in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(2)
+            make.left.equalTo(self.view)
+            make.right.equalTo(self.view)
+            make.bottom.equalTo(self.view)
+        }
+        
+        factTableView.dataSource = self
+        
         factTableView.register(FactCellTableViewCell.self, forCellReuseIdentifier: "FactCell")
         factTableView.estimatedRowHeight = 80
         factTableView.rowHeight = UITableViewAutomaticDimension
     }
     
     func updateUI() {
-        self.navigationBar.topItem?.title = responseData?.title
+        self.navigationItem.title = responseData?.title
         self.factTableView.reloadData()
     }
     
+    // MARK: - Data mannagement methods
     func fetchInfoData() {
         NetworkManger.getInfoData { (responseModel, error) in
             if error != nil{
@@ -62,22 +74,15 @@ class HomeViewController: UIViewController {
 
 }
 
+// MARK: - UITableViewDataSource methods
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return responseData?.infoArray?.count ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "FactCell", for: indexPath) as! FactCellTableViewCell
-//        if cell == nil {
-//            cell = FactCellTableViewCell(style: .default, reuseIdentifier: "FactCell")
-//        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FactCell", for: indexPath) as! FactCellTableViewCell
         cell.update(with: (responseData?.infoArray![indexPath.item])!)
         return cell
     }
 }
-
-extension HomeViewController: UITableViewDelegate{
-    
-}
-

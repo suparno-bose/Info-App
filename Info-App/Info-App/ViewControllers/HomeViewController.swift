cc1e4482204b7525e35673130819b0f814abe52d
Suparno <suparno.bose1@wipro.com>
2019-10-11 19:29:43 +0530

    Updated Home Screen with new Flat color.

--- Info-App/Info-App/ViewControllers/HomeViewController.swift
+++ Info-App/Info-App/ViewControllers/HomeViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SDWebImage
 import SnapKit
-
+import HexColors
 class HomeViewController: UIViewController {
 
     // MARK: - UI variables
@@ -32,7 +32,7 @@ class HomeViewController: UIViewController {
     
     // MARK: - UI related methods
     func setupUI(){
-        self.view.backgroundColor = UIColor(red: 0.3, green: 0.3, blue: 0.4, alpha: 1.0)
+        self.view.backgroundColor = UIColor("#34495e") // Wet Ashfalt
         
         factTableView = UITableView(frame: CGRect.zero)
         factTableView.backgroundColor = UIColor.clear

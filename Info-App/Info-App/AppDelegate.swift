e05e7e1714fc001e5a07518a2fc27b6ba717c964
Suparno <suparno.bose1@wipro.com>
2019-10-11 18:38:29 +0530

    Introduced Navigation controller to take the advantage of Navigation Bar

--- Info-App/Info-App/AppDelegate.swift
+++ Info-App/Info-App/AppDelegate.swift
@@ -15,9 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let homeViewControlleripad = HomeViewController()
+        let homeViewController = HomeViewController()
+        let rootNavigationController = UINavigationController(rootViewController: homeViewController)
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.rootViewController = homeViewControlleripad
+        self.window?.rootViewController = rootNavigationController
         self.window?.makeKeyAndVisible()
 
         return true
